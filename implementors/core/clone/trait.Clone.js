258c3d42afd97bb29240fd1d80387d677c0cf197
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-09-03 21:47:33 +0000

    Deploy Ekranos/nagiosplugin-rs to github.com/Ekranos/nagiosplugin-rs.git:gh-pages

---
+++ implementors/core/clone/trait.Clone.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["nagiosplugin"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html\" title=\"trait core::clone::Clone\">Clone</a> for <a class=\"enum\" href=\"nagiosplugin/enum.State.html\" title=\"enum nagiosplugin::State\">State</a>",synthetic:false,types:["nagiosplugin::State"]},{text:"impl&lt;T:&nbsp;<a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html\" title=\"trait core::clone::Clone\">Clone</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html\" title=\"trait core::clone::Clone\">Clone</a> for <a class=\"struct\" href=\"nagiosplugin/struct.SimpleMetric.html\" title=\"struct nagiosplugin::SimpleMetric\">SimpleMetric</a>&lt;T&gt; <span class=\"where fmt-newline\">where<br>&nbsp;&nbsp;&nbsp;&nbsp;T: <a class=\"trait\" href=\"nagiosplugin/trait.ToPerfString.html\" title=\"trait nagiosplugin::ToPerfString\">ToPerfString</a> + <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html\" title=\"trait core::clone::Clone\">Clone</a>,&nbsp;</span>",synthetic:false,types:["nagiosplugin::SimpleMetric"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ implementors/core/default/trait.Default.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["nagiosplugin"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"nagiosplugin/struct.Resource.html\" title=\"struct nagiosplugin::Resource\">Resource</a>",synthetic:false,types:["nagiosplugin::Resource"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
