4364ac25b02448f23e6c84a05e1a4b17995ee200
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-10-19 19:58:57 +0000

    Deploy Ekranos/nagiosplugin-rs to github.com/Ekranos/nagiosplugin-rs.git:gh-pages

--- implementors/core/cmp/trait.PartialEq.js
+++ implementors/core/cmp/trait.PartialEq.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["nagiosplugin"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html\" title=\"trait core::cmp::PartialEq\">PartialEq</a>&lt;<a class=\"enum\" href=\"nagiosplugin/enum.State.html\" title=\"enum nagiosplugin::State\">State</a>&gt; for <a class=\"enum\" href=\"nagiosplugin/enum.State.html\" title=\"enum nagiosplugin::State\">State</a>",synthetic:false,types:["nagiosplugin::State"]},];
+implementors["nagiosplugin"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html\" title=\"trait core::cmp::PartialEq\">PartialEq</a> for <a class=\"enum\" href=\"nagiosplugin/enum.State.html\" title=\"enum nagiosplugin::State\">State</a>",synthetic:false,types:["nagiosplugin::State"]},];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
