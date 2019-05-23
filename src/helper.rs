0914ee57f086d58fba16cf6f9fcaddbfd5f7b92e
Marvin Lee Fimmel <kontakt@ekranos.me>
2019-05-23 19:10:51 +0200

    Added missing docs for helper::safe_run and helper::safe_run_with_state

--- src/helper.rs
+++ src/helper.rs
@@ -3,10 +3,14 @@ use std::process::exit;
 
 use crate::State;
 
+/// Runs the given closure and exits with a State::Critical after printing out
+/// the error message if the Result contains an Err.
 pub fn safe_run<F: Fn() -> Result<(), Box<Error>>>(closure: F) {
     safe_run_with_state(closure, State::Critical);
 }
 
+/// Runs the given closure and exits with the given State after printing out
+/// the error message if the Result contains an Err.
 pub fn safe_run_with_state<F: Fn() -> Result<(), Box<Error>>>(closure: F, error_state: State) {
     if let Err(e) = closure() {
         println!("CRITICAL: {}", e.to_string());
