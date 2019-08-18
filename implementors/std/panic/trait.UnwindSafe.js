935aeb802776728aca99a429ba425e7e7e6a94eb
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-08-18 15:21:05 +0000

    Deploy Ekranos/nagiosplugin-rs to github.com/Ekranos/nagiosplugin-rs.git:gh-pages

---
+++ implementors/std/panic/trait.UnwindSafe.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["nagiosplugin"] = [{text:"impl !<a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/panic/trait.UnwindSafe.html\" title=\"trait std::panic::UnwindSafe\">UnwindSafe</a> for <a class=\"struct\" href=\"nagiosplugin/struct.Resource.html\" title=\"struct nagiosplugin::Resource\">Resource</a>",synthetic:true,types:["nagiosplugin::Resource"]},{text:"impl&lt;T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/panic/trait.UnwindSafe.html\" title=\"trait std::panic::UnwindSafe\">UnwindSafe</a> for <a class=\"struct\" href=\"nagiosplugin/struct.PartialOrdMetric.html\" title=\"struct nagiosplugin::PartialOrdMetric\">PartialOrdMetric</a>&lt;T&gt; <span class=\"where fmt-newline\">where<br>&nbsp;&nbsp;&nbsp;&nbsp;T: <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/panic/trait.UnwindSafe.html\" title=\"trait std::panic::UnwindSafe\">UnwindSafe</a>,&nbsp;</span>",synthetic:true,types:["nagiosplugin::PartialOrdMetric"]},{text:"impl&lt;T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/panic/trait.UnwindSafe.html\" title=\"trait std::panic::UnwindSafe\">UnwindSafe</a> for <a class=\"struct\" href=\"nagiosplugin/struct.SimpleMetric.html\" title=\"struct nagiosplugin::SimpleMetric\">SimpleMetric</a>&lt;T&gt; <span class=\"where fmt-newline\">where<br>&nbsp;&nbsp;&nbsp;&nbsp;T: <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/panic/trait.UnwindSafe.html\" title=\"trait std::panic::UnwindSafe\">UnwindSafe</a>,&nbsp;</span>",synthetic:true,types:["nagiosplugin::SimpleMetric"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/panic/trait.UnwindSafe.html\" title=\"trait std::panic::UnwindSafe\">UnwindSafe</a> for <a class=\"enum\" href=\"nagiosplugin/enum.State.html\" title=\"enum nagiosplugin::State\">State</a>",synthetic:true,types:["nagiosplugin::State"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
