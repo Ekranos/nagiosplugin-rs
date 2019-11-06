c073afcdae5db05a84bf5a853bc915f4190033e2
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-11-06 14:11:56 +0000

    Deploy Ekranos/nagiosplugin-rs to github.com/Ekranos/nagiosplugin-rs.git:gh-pages

---
+++ implementors/core/marker/trait.StructuralPartialEq.js
@@ -0,0 +1,9 @@
+(function() {var implementors = {};
+implementors["nagiosplugin"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.StructuralPartialEq.html\" title=\"trait core::marker::StructuralPartialEq\">StructuralPartialEq</a> for <a class=\"enum\" href=\"nagiosplugin/enum.State.html\" title=\"enum nagiosplugin::State\">State</a>",synthetic:false,types:["nagiosplugin::State"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        })()
--- implementors/nagiosplugin/trait.ResourceMetric.js
+++ implementors/nagiosplugin/trait.ResourceMetric.js
@@ -6,5 +6,4 @@ implementors["nagiosplugin"] = [];
             } else {
                 window.pending_implementors = implementors;
             }
-        
-})()
+        })()
