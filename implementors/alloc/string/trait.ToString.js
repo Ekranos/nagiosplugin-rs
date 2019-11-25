68e1452bdccd2e21ea6017b9ae25ded8c774bbba
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-11-25 11:29:08 +0000

    Deploy Ekranos/nagiosplugin-rs to github.com/Ekranos/nagiosplugin-rs.git:gh-pages

--- implementors/alloc/string/trait.ToString.js
+++ implementors/alloc/string/trait.ToString.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["nagiosplugin"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/alloc/string/trait.ToString.html\" title=\"trait alloc::string::ToString\">ToString</a> for <a class=\"enum\" href=\"nagiosplugin/enum.State.html\" title=\"enum nagiosplugin::State\">State</a>",synthetic:false,types:["nagiosplugin::State"]},];
+implementors["nagiosplugin"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/alloc/string/trait.ToString.html\" title=\"trait alloc::string::ToString\">ToString</a> for <a class=\"enum\" href=\"nagiosplugin/enum.State.html\" title=\"enum nagiosplugin::State\">State</a>",synthetic:false,types:["nagiosplugin::State"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/alloc/string/trait.ToString.html\" title=\"trait alloc::string::ToString\">ToString</a> for <a class=\"enum\" href=\"nagiosplugin/enum.Unit.html\" title=\"enum nagiosplugin::Unit\">Unit</a>",synthetic:false,types:["nagiosplugin::Unit"]},];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
