a88bf3c3927a8b01ec6bf610d106dd6ab63b39b8
Marvin Lee Fimmel <kontakt@ekranos.me>
2021-03-01 12:52:15 +0100

    Updated safe_run functions to be used in async contexts

--- src/helper.rs
+++ src/helper.rs
@@ -2,25 +2,28 @@ use std::error::Error;
 use std::process::exit;
 
 use crate::State;
+use std::future::Future;
 
 /// Runs the given closure and exits with a State::Critical after printing out
 /// the error message if the Result contains an Err.
-pub fn safe_run<F, E>(closure: F)
+pub async fn safe_run<F, E, R>(closure: F)
 where
-    F: Fn() -> Result<(), E>,
+    F: Fn() -> R,
     E: Error + Sized,
+    R: Future<Output=Result<(), E>>,
 {
-    safe_run_with_state(closure, State::Critical);
+    safe_run_with_state(closure, State::Critical).await;
 }
 
 /// Runs the given closure and exits with the given State after printing out
 /// the error message if the Result contains an Err.
-pub fn safe_run_with_state<F, E>(closure: F, error_state: State)
+pub async fn safe_run_with_state<F, E, R>(closure: F, error_state: State)
 where
-    F: Fn() -> Result<(), E>,
+    F: Fn() -> R,
     E: Error + Sized,
+    R: Future<Output=Result<(), E>>,
 {
-    if let Err(e) = closure() {
+    if let Err(e) = closure().await {
         println!("{}: {}", error_state.to_string(), e);
         exit(error_state.exit_code());
     }
