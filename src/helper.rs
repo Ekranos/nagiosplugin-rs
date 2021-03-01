eb4a1bc736f6627ab492fdd5411407a848bf5f99
Marvin Lee Fimmel <kontakt@ekranos.me>
2021-03-01 12:30:17 +0100

    More efficient and correct safe_run methods Bumped version to 0.2.0

--- src/helper.rs
+++ src/helper.rs
@@ -5,15 +5,23 @@ use crate::State;
 
 /// Runs the given closure and exits with a State::Critical after printing out
 /// the error message if the Result contains an Err.
-pub fn safe_run<F: Fn() -> Result<(), Box<dyn Error>>>(closure: F) {
+pub fn safe_run<F, E>(closure: F)
+where
+    F: Fn() -> Result<(), E>,
+    E: Error + Sized,
+{
     safe_run_with_state(closure, State::Critical);
 }
 
 /// Runs the given closure and exits with the given State after printing out
 /// the error message if the Result contains an Err.
-pub fn safe_run_with_state<F: Fn() -> Result<(), Box<dyn Error>>>(closure: F, error_state: State) {
+pub fn safe_run_with_state<F, E>(closure: F, error_state: State)
+where
+    F: Fn() -> Result<(), E>,
+    E: Error + Sized,
+{
     if let Err(e) = closure() {
-        println!("CRITICAL: {}", e.to_string());
+        println!("{}: {}", error_state.to_string(), e);
         exit(error_state.exit_code());
     }
 }
